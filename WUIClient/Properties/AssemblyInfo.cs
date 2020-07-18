78ab8c30739a3eb381e8f130f2e5dad5b4898244
waleedalqalaf <waleedalqalaf@hotmail.com>
2020-07-19 00:40:19 +0300

    Added version info RELEASE 0.0.1

--- WUIClient/Properties/AssemblyInfo.cs
+++ WUIClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WUILibrary")]
-[assembly: AssemblyProduct("WUILibrary")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("WUIClient")]
+[assembly: AssemblyProduct("WUIClient")]
+[assembly: AssemblyConfiguration("This is the multiplayer game engine client, waleedalqalaf.itch.io/wuiengine")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyCompany("Potatoxel")]
+[assembly: AssemblyCopyright("Copyright © Potatoxel. 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
